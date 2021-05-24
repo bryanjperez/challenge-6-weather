9129d2705a97640f3e07221dc1e2e0186f4632b9
Bryan Perez <bryanjperez@gmail.com>
2021-05-23 23:33:08 -0400

    updates to JS and HTML -- work in progress

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,60 @@
+// var searchButton = $('.btn');
+
+// searchButton.on('click', function () {
+// var searchBox = $('#search-field')
+
+// var searchText = searchBox.val();
+// event.preventDefault();
+// console.log(searchText)
+
+
+//  var APIkey = '843fa40ad68a96668befb0da86d9b44b'
+
+
+// var myURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&units=imperial&appid=" + APIkey
+
+// fetch(myURL)
+// .then((res) => {
+// return res.json()
+// })
+// .then((data) => {
+// console.log(data);
+
+// var cityName = $('#city-name');
+// cityName.text(data.name)
+
+// var cityTemp = $('#city-temp');
+// cityTemp.text(data.main.temp)
+
+// })
+
+
+// })
+// var searchBox = $('#search-field');
+// var searchText = searchBox.val();
+// var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+var APIkey = '843fa40ad68a96668befb0da86d9b44b';
 var searchButton = $('.btn');
-
-searchButton.on('click', function () {
-    var searchBox = $('#search-field')
-
-    var searchText = searchBox.val();
-    event.preventDefault();
-    console.log(searchText)
+var searchBox = $('#search-field');
+var searchInput = searchBox.val();
 
 
-    var APIkey = '843fa40ad68a96668befb0da86d9b44b'
 
 
-    var myURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=" + APIkey
-
-    fetch(myURL)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            console.log(data);
-
-            var cityName = $('#city-name');
-            cityName.text(data.name)
-
-            var cityTemp = $('#city-temp');
-            cityTemp.text(data.main.temp)
-
-        })
+$.getJSON(
+    "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&units=imperial" + APIkey,
+    function (data) {
+        // var searchButton = $('.btn');
+        // var searchBox = $('.textInput');
+        // var searchInput = searchBox.val();
+        var temp = Math.floor(data.main.temp);
+        var weather = data.weather[0].main;
+        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    })
 
 
-})
+// ERROR MESSAGES APPEAR TO BE BECAUSE OF THE TOKEN KEY. 401 ERROR = 
+// The HTTP 401 Unauthorized client error status response code indicates 
+// that the request has not been applied because it lacks valid 
+// authentication credentials for the target resource
